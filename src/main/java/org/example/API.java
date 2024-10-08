c583c029651c7abe2e78688bc1855a182398ecca
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2024-10-08 13:09:20 -0500

    se amplia las  opciones de conversion de monedas

--- src/main/java/org/example/API.java
+++ src/main/java/org/example/API.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public class API {
 
-    public static Map<String,Object> getMapApi(String base_code,String target_code, double monto){
+    public static Map<String,Object> getMapFromApi(String base_code,String target_code, double monto){
         try{
             // utilizamos Client
             HttpClient client = HttpClient.newHttpClient();
@@ -27,5 +27,37 @@ public class API {
             System.out.println(e);
         }
         return null;
+    }
+    public static void getMenu(){
+        StringBuilder menu = new StringBuilder();
+
+        menu.append("************************************************************").append("\n");
+
+        menu.append("Sea Bienvenido/a al conversor de Monedas =]").append("\n\n");
+
+        menu.append("(1) Dólar => Peso Argentino").append("\n");
+        menu.append("(2) Peso Argentino => Dólar").append("\n");
+        menu.append("(3) Dólar => Real Brasileño").append("\n");
+        menu.append("(4) Real Brasileño => Dólar").append("\n");
+        menu.append("(5) Dólar => Peso Colombiano").append("\n");
+        menu.append("(6) Peso Colombiano => Dólar").append("\n");
+        menu.append("(7) Dólar => Euro").append("\n");
+        menu.append("(8) Euro => Dólar").append("\n");
+        menu.append("(9) Dólar => Libra Esterlina").append("\n");
+        menu.append("(10) Libra Esterlina => Dólar").append("\n");
+        menu.append("(11) Dólar => Yen Japonés").append("\n");
+        menu.append("(12) Yen Japonés => Dólar").append("\n");
+        menu.append("(13) Euro => Peso Argentino").append("\n");
+        menu.append("(14) Peso Argentino => Euro").append("\n");
+        menu.append("(15) Euro => Real Brasileño").append("\n");
+        menu.append("(16) Real Brasileño => Euro").append("\n");
+        menu.append("(17) revisar el historial").append("\n");
+        menu.append("(18) salir").append("\n\n");
+
+        menu.append("Elija una opción válida: ").append("\n");
+
+        menu.append("****************************************************************");
+
+        System.out.println(menu);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,32 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-
-    public static void getMenu(){
-        StringBuilder menu = new StringBuilder();
-
-        menu.append("************************************************************").append("\n");
-
-        menu.append("Sea Bienvenido/a al converso de Monedas =]").append("\n\n");
-
-        menu.append("(1) Dólar => Peso Argentino").append("\n");
-        menu.append("(2) Peso Argentino => Dólar").append("\n");
-        menu.append("(3) Dólar => Real Brasileño").append("\n");
-        menu.append("(4) Real Brasileño => Dolar").append("\n");
-        menu.append("(5) Dolar => Peso Colombiano").append("\n");
-        menu.append("(6) Peso colombiano => Dolar").append("\n");
-        menu.append("(7) revisar el historial").append("\n");
-        menu.append("(8) Salir").append("\n\n");
-
-        menu.append("Elija una opcion valida: ").append("\n");
-
-        menu.append("****************************************************************");
-        System.out.println(menu);
-    }
-
     public static void main(String[] args) {
         try {
-
             LocalDateTime time;
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy dd MMMM-HH:mm:ss");
             String resultado;
@@ -51,8 +27,8 @@ public class Main {
 
             int opcion = 0;
 
-            while (opcion != 8){
-                getMenu();
+            while (opcion != 18){
+                API.getMenu();
                 opcion = Integer.parseInt(input.readLine());
 
                 switch (opcion){
@@ -64,8 +40,8 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         double monto = Double.parseDouble(input.readLine());
-                        Map<String, Object> map = API.getMapApi("USD","ARS", monto);
-                        System.out.println("El monto " + monto + "[USD] en pesos argentinos es " + map.get("conversion_result") + " [ARS]");
+                        Map<String, Object> map = API.getMapFromApi("USD","ARS", monto);
+                        System.out.println("El monto " + monto + " [USD] en pesos argentinos es " + map.get("conversion_result") + " [ARS]");
                         break;
                     case 2:
 
@@ -75,8 +51,8 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         monto = Double.parseDouble(input.readLine());
-                        map = API.getMapApi("ARS","USD", monto);
-                        System.out.println("El monto " + monto + "[ARS] en dolares es " + map.get("conversion_result") + " [USD]");
+                        map = API.getMapFromApi("ARS","USD", monto);
+                        System.out.println("El monto " + monto + " [ARS] en dolares es " + map.get("conversion_result") + " [USD]");
                         break;
                     case 3:
 
@@ -86,8 +62,8 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         monto = Double.parseDouble(input.readLine());
-                        map = API.getMapApi("USD", "BRL", monto);
-                        System.out.println("El monto " + monto + "[USD] en real brasileño es " + map.get("conversion_result") + " [BRL]");
+                        map = API.getMapFromApi("USD", "BRL", monto);
+                        System.out.println("El monto " + monto + " [USD] en real brasileño es " + map.get("conversion_result") + " [BRL]");
                         break;
                     case 4:
 
@@ -97,8 +73,8 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         monto = Double.parseDouble(input.readLine());
-                        map = API.getMapApi("BRL","USD", monto);
-                        System.out.println("El monto " + monto + "[BRL] en dolares es " + map.get("conversion_result") + " [USD]");
+                        map = API.getMapFromApi("BRL","USD", monto);
+                        System.out.println("El monto " + monto + " [BRL] en dolares es " + map.get("conversion_result") + " [USD]");
                         break;
                     case 5:
 
@@ -108,8 +84,8 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         monto = Double.parseDouble(input.readLine());
-                        map = API.getMapApi("USD","COP", monto);
-                        System.out.println("El monto " + monto + "[USD] en peso colombiano es " + map.get("conversion_result") + " [COP]");
+                        map = API.getMapFromApi("USD","COP", monto);
+                        System.out.println("El monto " + monto + " [USD] en peso colombiano es " + map.get("conversion_result") + " [COP]");
                         break;
                     case 6:
 
@@ -119,15 +95,115 @@ public class Main {
 
                         System.out.print("Ingrese el valor que desea convertir: ");
                         monto = Double.parseDouble(input.readLine());
-                        map = API.getMapApi("COP","USD", monto);
-                        System.out.println("El monto " + monto + "[COP] en dolares es " + map.get("conversion_result") + " [USD]");
+                        map = API.getMapFromApi("COP","USD", monto);
+                        System.out.println("El monto " + monto + " [COP] en dolares es " + map.get("conversion_result") + " [USD]");
                         break;
                     case 7:
-                        System.out.println("El historial: ");
-                        historial.put(nombre, fechaOpcion);
-                        System.out.println(historial);
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("USD","EUR", monto);
+                        System.out.println("El monto " + monto + " [USD] en eures es " + map.get("conversion_result") + " [EUR]");
                         break;
                     case 8:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("EUR","USD", monto);
+                        System.out.println("El monto " + monto + " [EUR] en dolares es " + map.get("conversion_result") + " [USD]");
+                        break;
+                    case 9:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("USD","GBP", monto);
+                        System.out.println("El monto " + monto + " [USD] en libras esterlinas es " + map.get("conversion_result") + " [GBP]");
+                        break;
+                    case 10:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("GBP","USD", monto);
+                        System.out.println("El monto " + monto + " [GBP] en dolares es " + map.get("conversion_result") + " [USD]");
+                        break;
+                    case 11:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("USD","JPY", monto);
+                        System.out.println("El monto " + monto + " [USD] en yueanes japones es " + map.get("conversion_result") + " [JPY]");
+                        break;
+                    case 12:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("JPY","USD", monto);
+                        System.out.println("El monto " + monto + " [JPY] en dolares es " + map.get("conversion_result") + " [USD]");
+                        break;
+                    case 13:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("EUR","ARS", monto);
+                        System.out.println("El monto " + monto + " [EUR] en pesos argentinos es " + map.get("conversion_result") + " [ARS]");
+                        break;
+                    case 14:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("ARS","EUR", monto);
+                        System.out.println("El monto " + monto + " [ARS] en euros es " + map.get("conversion_result") + " [EUR]");
+                        break;
+                    case 15:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("EUR","BRL", monto);
+                        System.out.println("El monto " + monto + " [EUR] en reales brasileños es " + map.get("conversion_result") + " [BRL]");
+                        break;
+                    case 16:
+                        time = LocalDateTime.now();
+                        resultado = formatter.format(time);
+                        fechaOpcion.put(resultado, opcion);
+
+                        System.out.print("Ingrese el valor que desea convertir: ");
+                        monto = Double.parseDouble(input.readLine());
+                        map = API.getMapFromApi("BRL","EUR", monto);
+                        System.out.println("El monto " + monto + " [BRL] en euros es " + map.get("conversion_result") + " [EUR]");
+                        break;
+                    case 17:
+                        System.out.println("Historial");
+                        historial.put(nombre,fechaOpcion);
+                        System.out.println(historial);
+                        break;
+                    case 18:
                         System.out.println("Gracias por utilizar mi conversion de monedas :3");
                         break;
                     default:
